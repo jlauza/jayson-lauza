54389efd6efd113c1bccb4f04f2b1f8a8cd905c2
jlauza <lauzajayson@gmail.com>
2025-01-18 18:09:38 +0800

    add logo in project page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:filter-[invert(1)]"
+          src="/pronetsuite-light.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
         <h1 className="text-3xl sm:text-4xl font-bold">Projects</h1>
         <div className="flex items-center gap-2">
           <input
