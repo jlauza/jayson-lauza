05b18cb08a084ea42e4868675f61cfaea6cd91fe
jlauza <lauzajayson@gmail.com>
2025-01-18 17:36:15 +0800

    initial update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+import {
+  DoubleArrowRightIcon,
+  EnvelopeClosedIcon,
+} from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className="dark:filter-[invert(1)]"
+          src="/pronetsuite-light.png"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -14,38 +19,34 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
+            This website is intented to display the project collections
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">Creator of this site is Jayson Lauza</li>
+          <li className="mb-2">
+            See the author's{" "}
+            <Link href="https://github.com/jlauza" target="_blank">
+              GitHub profile <DoubleArrowRightIcon className="inline" />
+            </Link>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-primary text-foreground gap-2 hover:bg-secondary dark:hover:bg-secondary text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/jlauzas-projects"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Explore Projects
+            <DoubleArrowRightIcon className="inline" />
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#cecece] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            <EnvelopeClosedIcon className="inline mr-2" /> Contact Us
           </a>
         </div>
       </main>
@@ -63,7 +64,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Projects
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +79,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          Testimonails
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -93,7 +94,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Contact Us
         </a>
       </footer>
     </div>
