6828f9d95f68378d9a1b69246ad79e33f0f408a9
jlauza <lauzajayson@gmail.com>
2025-01-20 00:35:34 +0800

    global footer

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import GlobalFooter from "../components/global-footer";
 
 const ProjectsPage = () => {
   return (
@@ -71,6 +72,7 @@ const ProjectsPage = () => {
           </div>
         </div>
       </main>
+      <GlobalFooter />
     </div>
   );
 };
